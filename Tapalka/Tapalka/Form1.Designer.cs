9b2a79bda07c8453e492e2d45f3871dcb3e2da00
lol198273951-ui <lol198273951@gmail.com>
2026-02-28 03:41:16 +0800

    Gen1: таймер, тыкать enter постоянно, только один уровень сложности

---
+++ Tapalka/Tapalka/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Tapalka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Пропустить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 256);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Tapalka/Tapalka/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tapalka
+{
+    public partial class Form1 : Form
+    {
+        private List<string> words1 = new List<string>
+        {
+            "кот", "дом", "лес", "река", "гора", "машина", "компьютер",
+            "программа", "код", "разработка", "скорость", "печать", "текст",
+            "клавиатура", "мышь", "экран", "окно", "кнопка", "файл", "папка"
+        };
+        private List<string> words2 = new List<string>
+        {
+            "кот", "дом", "лес", "река", "гора", "машина", "компьютер",
+            "программа", "код", "разработка", "скорость", "печать", "текст",
+            "клавиатура", "мышь", "экран", "окно", "кнопка", "файл", "папка"
+        };
+        private List<string> words3 = new List<string>
+        {
+            "кот", "дом", "лес", "река", "гора", "машина", "компьютер",
+            "программа", "код", "разработка", "скорость", "печать", "текст",
+            "клавиатура", "мышь", "экран", "окно", "кнопка", "файл", "папка"
+        };
+
+        private Random random = new Random();
+        private string currentWord;
+        private int score = 0;
+        private int timeLeft = 30;
+        private bool isGameActive = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Нажмите СТАРТ";
+            button2.Enabled = false;
+            textBox1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+        private void StartGame()
+        {
+            score = 0;
+            timeLeft = 30;
+            isGameActive = true;
+
+            label2.Text = "Правильно: 0 слов";
+            label3.Text = "Осталось: " + timeLeft + " сек";
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+            textBox1.Enabled = true;
+            textBox1.Clear();
+            textBox1.Focus();
+
+            NextWord();
+
+            timer1.Start();
+        }
+        private void NextWord()
+        {
+            currentWord = words1[random.Next(words1.Count)];
+            label1.Text = currentWord;
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!isGameActive) return;
+
+            if (textBox1.Text.Trim().ToLower() == currentWord.ToLower())
+            {
+                score++;
+                label2.Text = $"Правильно: {score} слов";
+            }
+
+            NextWord();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft--;
+            label3.Text = $"Осталось: {timeLeft} сек";
+
+            if (timeLeft <= 0)
+            {
+                EndGame();
+            }
+        }
+        private void EndGame()
+        {
+            timer1.Stop();
+            isGameActive = false;
+
+            MessageBox.Show($"Время вышло!\nПравильно введено слов: {score}\nСкорость: {score} WPM",
+                          "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            button1.Enabled = true;
+            button2.Enabled = false;
+            textBox1.Enabled = false;
+            label1.Text = "Нажмите СТАРТ";
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                button2_Click(sender, e);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
