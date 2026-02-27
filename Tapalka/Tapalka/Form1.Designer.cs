954f6f860cb6331f3c201b1a23997041f38b6b06
lol198273951-ui <lol198273951@gmail.com>
2026-02-28 04:08:39 +0800

    Gen2 - добавлены 3 уровня сложности и автоматическая проверка слов

--- Tapalka/Tapalka/Form1.Designer.cs
+++ Tapalka/Tapalka/Form1.Designer.cs
@@ -36,6 +36,11 @@
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -102,11 +107,72 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(234, 95);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Простой";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(234, 118);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Средний";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(234, 141);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(72, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Сложный";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(217, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Уровни сложности";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(227, 174);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "СТОП";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(241, 256);
+            this.ClientSize = new System.Drawing.Size(348, 224);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -130,6 +196,11 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Tapalka/Tapalka/Form1.cs
+++ Tapalka/Tapalka/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Tapalka
@@ -20,17 +14,21 @@ namespace Tapalka
         };
         private List<string> words2 = new List<string>
         {
-            "кот", "дом", "лес", "река", "гора", "машина", "компьютер",
-            "программа", "код", "разработка", "скорость", "печать", "текст",
-            "клавиатура", "мышь", "экран", "окно", "кнопка", "файл", "папка"
+            "машина", "компьютер", "программа", "клавиатура", "монитор", "телефон",
+            "телевизор", "холодильник", "микроволновка", "стиральная", "посудомойка",
+            "библиотека", "университет", "школа", "магазин", "больница", "аптека",
+            "ресторан", "кафе", "столовая", "парк", "сквер", "площадь", "улица"
         };
         private List<string> words3 = new List<string>
         {
-            "кот", "дом", "лес", "река", "гора", "машина", "компьютер",
-            "программа", "код", "разработка", "скорость", "печать", "текст",
-            "клавиатура", "мышь", "экран", "окно", "кнопка", "файл", "папка"
+            "программирование", "администрирование", "конфигурирование", "инкапсуляция",
+            "наследование", "полиморфизм", "деструктор", "конструктор", "интерфейс",
+            "абстракция", "делегирование", "сериализация", "десериализация", "многопоточность",
+            "синхронизация", "асинхронность", "параллелизм", "конкурентность", "оптимизация",
+            "рефакторинг", "документирование", "тестирование", "отладка", "дебаггинг"
         };
 
+        private List<string> currentWords;
         private Random random = new Random();
         private string currentWord;
         private int score = 0;
@@ -47,15 +45,35 @@ namespace Tapalka
             button2.Enabled = false;
             textBox1.Enabled = false;
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void NextWord()
         {
-            StartGame();
+            if (currentWords != null)
+            {
+                currentWord = currentWords[random.Next(currentWords.Count)];
+                label1.Text = currentWord;
+                textBox1.Clear();
+                textBox1.Focus();
+            }
         }
         private void StartGame()
         {
+            if (radioButton2.Checked)
+            {
+                currentWords = words2;
+                timeLeft = 45;
+            }
+            else if (radioButton3.Checked)
+            {
+                currentWords = words3;
+                timeLeft = 30;
+            }
+            else
+            {
+                currentWords = words1;
+                timeLeft = 60;
+            }
+
             score = 0;
-            timeLeft = 30;
             isGameActive = true;
 
             label2.Text = "Правильно: 0 слов";
@@ -64,6 +82,11 @@ namespace Tapalka
             button1.Enabled = false;
             button2.Enabled = true;
             textBox1.Enabled = true;
+
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+            radioButton3.Enabled = false;
+
             textBox1.Clear();
             textBox1.Focus();
 
@@ -71,13 +94,13 @@ namespace Tapalka
 
             timer1.Start();
         }
-        private void NextWord()
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            currentWord = words1[random.Next(words1.Count)];
-            label1.Text = currentWord;
-            textBox1.Clear();
-            textBox1.Focus();
+            StartGame();
         }
+        
+        
         private void button2_Click(object sender, EventArgs e)
         {
             if (!isGameActive) return;
@@ -123,9 +146,35 @@ namespace Tapalka
             }
         }
 
+        private void EndGame2()
+        {
+            timer1.Stop();
+            isGameActive = false;
+
+            MessageBox.Show($"Время вышло!\nПравильно введено слов: {score}\nСкорость: {score} WPM",
+                          "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            button1.Enabled = true;
+            button2.Enabled = false;
+            textBox1.Enabled = false;
+            label1.Text = "Нажмите СТАРТ";
+
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+        }
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+            if (!isGameActive) return;
+
+            if (textBox1.Text.Trim().ToLower() == currentWord.ToLower())
+            {
+                score++;
+                label2.Text = $"Правильно: {score} слов";
+
+                NextWord();
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -143,5 +192,29 @@ namespace Tapalka
 
         }
 
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EndGame2();
+        }
     }
 }
